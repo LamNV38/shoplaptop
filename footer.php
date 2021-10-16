4e48d2c7e697c01e6ae17b06ed847d28aa36e5bd
NguyenLamTh07 <92618998+NguyenLamTh07@users.noreply.github.com>
2021-10-16 12:43:02 +0700

    add shoplaptop

---
+++ footer.php
@@ -0,0 +1,10 @@
+<embed id="Advertisement" 
+width="960" height="100" 
+flashvars="clickTARGET=_blank&clickTAG=#" 
+allowscriptaccess="always"
+wmode="transparent" quality="high" 
+name="Advertisement" style="admincp" 
+src="images/quangcao.swf" 
+type="application/x-shockwave-flash">
+Đồ án Website kinh doanh máy tính xách tay trực tuyến</br>
+Người thực hiện: Nguyễn Văn Lâm -Lớp Th24.07 -MSV:19171865- Email:lam11102001@gmail.com - SĐT:0989078891
